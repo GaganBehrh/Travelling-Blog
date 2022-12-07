0adc092ea450349ee9c283a87a1a94cf32e73f29
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-12-07 15:16:39 +0000

    Setings.py moified herku app name added

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['newblogappgb.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['newsblog12', 'localhost']
 
 
 # Application definition
