4cb38b22b11de61e04205be917b64c9bfe3b6036
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-12-11 16:15:59 +0000

    Debug set to False

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['newsblog12.herokuapp.com', 'localhost']
 
