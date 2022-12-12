477b1f44a7960bafb007cd0f8d186292805df764
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-12-12 14:38:25 +0000

    First view created

--- codestar/urls.py
+++ codestar/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("newsblog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
+
 
--- newsblog/views.py
+++ newsblog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
-class Postlist(generic.ListView):
+class PostList(generic.ListView):
     model=Post
     querset=Post.objects.filter(status=1).order_by('-created_on')
     template_name='index.html'
