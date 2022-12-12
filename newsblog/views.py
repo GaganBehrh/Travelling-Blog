bad04731bcff048e990774230333e8fb21c1ac9a
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-12-12 13:55:15 +0000

    creating the first view

--- newsblog/views.py
+++ newsblog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+class Postlist(generic.ListView):
+    model=Post
+    querset=Post.objects.filter(status=1).order_by('-created_on')
+    template_name='index.html'
+    paginate_by=6
 
 # Create your views here.
