f794da5cd1f5230cd6a575e589c0deacc615b838
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-12-14 14:27:41 +0000

    Message alert added

--- codestar/settings.py
+++ codestar/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -60,6 +61,16 @@ INSTALLED_APPS = [
 SITE_ID=1
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_url='/'
+
+MESSAGE_TAGS={
+    messages.DEBUG:'alert-info',
+    messages.INFO:'alert-info',
+    messages.SUCCESS:'alert.success',
+    messages.WARNING:'alert-warning',
+    messages.ERROR:'alert-danger',
+
+
+}
 
 crispy_template_pack='bootstrap4'
 
