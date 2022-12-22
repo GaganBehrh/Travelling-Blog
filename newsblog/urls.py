f9ca455485c07415ab62fe333caee68485bf0073
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-12-22 15:27:10 +0000

    Manage posts button works

--- newsblog/urls.py
+++ newsblog/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
+    path('post_view/', views.PostView.as_view(), name='post_view'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('posts/<slug:slug>', views.PostView.as_view(), name='posts_view'),
-
 ]
