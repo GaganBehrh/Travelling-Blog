4067b36db358f191c9a7e5e0acffef8932489ed1
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-12-27 15:35:59 +0000

    prompt message added

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,10 @@
 nextdestination = document.getElementById("nextdestination");
 nextdestination.onclick = myFunction();
-var choice = 0;
+var choice;
 
 function myFunction() {
-    alert("Enter a number between 1 to 5?");
-    if (choice == 1)
+    prompt("Enter a number between 1 to 5?");
+    if (choice === 1)
         alert("You are going to France")
     else {
         alert("You are going to Germany")
