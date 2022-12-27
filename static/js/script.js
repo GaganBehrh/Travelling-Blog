83722f32da79037c5987f6b3233d19e94970afa3
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-12-27 15:45:22 +0000

    script.js completed

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,18 @@
-nextdestination = document.getElementById("nextdestination");
+/*nextdestination = document.getElementById("nextdestination");
 nextdestination.onclick = myFunction();
-var choice;
+var choice = 0;
 
 function myFunction() {
     prompt("Enter a number between 1 to 5?");
     if (choice === 1)
-        alert("You are going to France")
+        alert("You are going to France");
     else {
-        alert("You are going to Germany")
+        alert("You are going to Germany");
     }
 
-}
+}*/
+setTimeout(function () {
+    let messages = document.getElementById('msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 2500);
