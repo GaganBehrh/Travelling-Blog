2f5bfb9652848101b05f826fbdb2100e161ab370
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-12-27 15:21:08 +0000

    script.js added

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,13 @@
 nextdestination = document.getElementById("nextdestination");
+nextdestination.onclick = myFunction();
+var choice = 0;
+
+function myFunction() {
+    alert("Enter a number between 1 to 5?");
+    if (choice == 1)
+        alert("You are going to France")
+    else {
+        alert("You are going to Germany")
+    }
+
+}
