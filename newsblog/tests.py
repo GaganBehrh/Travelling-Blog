29cd22f348092326ff69c996f3fdaae892a3b366
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-01-04 17:41:16 +0000

    tests for PostView added

--- newsblog/tests.py
+++ newsblog/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from newsblog.views import PostView
+
 
 # Create your tests here.
+class TestUrls(SimpleTestCase):
+    def test_post(self):
+        url = reverse('post_view')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, PostView)
