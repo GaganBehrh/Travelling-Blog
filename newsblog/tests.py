b971205d5baa2a358935b774db5ecc0d7745a991
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-01-04 17:50:26 +0000

    PostView test added

--- newsblog/tests.py
+++ newsblog/tests.py
@@ -3,9 +3,10 @@ from django.urls import reverse, resolve
 from newsblog.views import PostView
 
 
+
 # Create your tests here.
 class TestUrls(SimpleTestCase):
     def test_post(self):
         url = reverse('post_view')
         print(resolve(url))
-        self.assertEquals(resolve(url).func, PostView)
+        self.assertEquals(resolve(url).func, views.PostView)
