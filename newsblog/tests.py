107a228e4b63ff89577090d56b694ccdf94fb0bd
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-01-05 05:48:37 +0000

    4 tests added to test_url.py

--- newsblog/tests.py
+++ newsblog/tests.py
@@ -1,11 +1,27 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from newsblog.views import PostView
+from newsblog.views import PostView, AddView, EditView, Delete,PostDetail, PostLike
 
 
-
-# Create your tests here.
 class TestUrls(SimpleTestCase):
-    def test_post(self):
+    def test_post_view(self):
         url = reverse('post_view')
         self.assertEquals(resolve(url).func.view_class, PostView)
+    
+    def test_add_view(self):
+        url = reverse('add')
+        self.assertEquals(resolve(url).func.view_class, AddView)
+    
+    def test_post_detail_view(self):
+        url = reverse('post_detail', args=['some-slug'])
+        self.assertEquals(resolve(url).func.view_class, PostDetail)
+    
+    def test_post_like_view(self):
+        url = reverse('post_like', args=['some-slug'])
+        self.assertEquals(resolve(url).func.view_class, PostLike)
+    
+   
+    
+    
+
+
