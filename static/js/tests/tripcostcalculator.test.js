843739d48eec2c7aaf89bd0bb93d207aab39d61e
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-01-05 16:31:36 +0000

    Basic tests working

---
+++ static/js/tests/tripcostcalculator.test.js
@@ -0,0 +1,8 @@
+const addition = require("../tripcostcalculator");
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+    });
+});
--- static/js/tripcostcalculator.test.js
+++
@@ -1,7 +0,0 @@
-const addition = require("../../tripcostcalculator");
-describe("Calculator", () => {
-            describe("Addition function", () => {
-                test("should return 42 for 20 + 22", () => {
-                    expect(addition(20, 22)).toBe(42);
-                });
-            });
