07d2b27f51fa97a88cdddecb88b2733e5e1cb011
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-01-06 06:49:47 +0000

    Number of days label created

--- static/js/tripcostcalculator.js
+++ static/js/tripcostcalculator.js
@@ -1,8 +1,17 @@
-let numberofdays = document.getElementById("num").value;
-console.log(numberofdays);
+/*let numberofdays = document.getElementById("num");
+console.log(numberofdays.value);*/
 
 function addition() {
+    let numberofdays = document.getElementById("num");
+    console.log(numberofdays.value);
     alert(numberofdays * 1000);
+}
+addition();
+
+function handleSubmit(event) {
+    event.preventDefault();
+    /*let cityentered=document.getElementById("cityentered");
+    search(cityentered.value);*/
 }
 
 // module.exports = addition;
