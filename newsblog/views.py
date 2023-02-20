6a5d98c1e028583c8bb1a32b46ce7b5c7c15b9c6
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-02-20 20:51:35 +0000

    Add View modified only selected fields included

--- newsblog/views.py
+++ newsblog/views.py
@@ -20,8 +20,8 @@ class PostView(generic.ListView):
     model = Post
     template_name = "post_view.html"
     context_object_name = 'post_list'
-    
-    
+
+ 
 #class AddView(LoginRequiredMixin,CreateView)
 class AddView(LoginRequiredMixin, CreateView):# add def get or post method to override form_valid in django create view,override form view
     model = Post
