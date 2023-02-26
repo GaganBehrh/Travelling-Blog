4b8445d3a93e4dd941ffb683e9fac61cc9ba4200
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-02-26 06:55:49 +0000

    Contact Model added to admin.py

--- newsblog/admin.py
+++ newsblog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Contact
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -22,6 +22,8 @@ class CommentAdmin(admin.ModelAdmin):
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
 
+
+admin.site.register(Contact)
 
 #migrations command;python manage.py makemigrations,python manage.py migrate
 
