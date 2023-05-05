92c76e1201cbf7a21ef16f1905a7e206f2775761
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-05-05 12:28:49 +0000

    User can leave individual comments now and approval can also be carried out

--- newsblog/views.py
+++ newsblog/views.py
@@ -62,7 +62,7 @@ class Delete(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('post_view')
 
 
-class PostDetail(LoginRequiredMixin, View):
+class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
@@ -84,9 +84,8 @@ class PostDetail(LoginRequiredMixin, View):
                
             },
         )
-    
-    def post(self, request, slug, *args, **kwargs):
 
+    def post(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
@@ -94,7 +93,7 @@ class PostDetail(LoginRequiredMixin, View):
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        comment_form = CommentForm(data=request.Post)
+        comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
@@ -116,6 +115,7 @@ class PostDetail(LoginRequiredMixin, View):
             },
         )
 
+    
 
 class PostLike(LoginRequiredMixin, View):
     def post(self, request, slug, *args, **kwargs):
